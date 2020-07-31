c01a1655c9d3db9973609f2aa5f674e37fe1c87f
shinji <s.t.nkmss@gmail.com>
2020-07-31 09:19:22 +0900

    installed debug-toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,17 @@
 from .base import *
 
 
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+
+    INSTALLED_APPS += {
+        'debug_toolbar',
+    }
+    MIDDLEWARE += {
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    }
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
